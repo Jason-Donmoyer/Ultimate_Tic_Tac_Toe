a7845941a82cfc40931346ab6e0197a521af5334
theDuke523 <j.donmoyer523@gmail.com>
2018-04-04 04:46:49 -0400

    added functions for start of game and created player objects

--- tic-tac-toe-v3/js/tictacapp.js
+++ tic-tac-toe-v3/js/tictacapp.js
@@ -3,8 +3,18 @@
 const gameScreen = document.querySelector('#board');
 const startScreen = document.querySelector('#start');
 const startButton = document.querySelector('.button');
-const playerOne = document.querySelector('#player1');
-const playerTwo = document.querySelector('#player2');
+//const playerOne = document.querySelector('#player1');
+//const playerTwo = document.querySelector('#player2');
+
+let PlayerOne = {
+	isTurn: document.querySelector('#player1'),
+	isActive: false
+};
+
+let PlayerTwo = {
+	isTurn: document.querySelector('#player2'),
+	isActive: false
+};
 
 // Hides the game board on start
 gameScreen.style.display = "none";
@@ -15,13 +25,54 @@ startButton.addEventListener('click', function() {
 	gameScreen.style.display = '';
 });
 
+// Function to pick who goes first
 function goesFirst () {
 	let randInt = Math.floor(Math.random() * 2) + 1;
 	if (randInt === 1) {
-		playerOne.classList.add('active');
+		PlayerOne.isTurn.classList.add('active');
+		PlayerOne.isActive = true;
 	} else {
-		playerTwo.classList.add('active');
+		PlayerTwo.isTurn.classList.add('active');
+		PlayerTwo.isActive = true;
 	}
 }
+
+// Function to change turns
+function switchTurn () {
+	if (PlayerTwo.isActive) {
+		PlayerOne.isTurn.classList.remove('active');
+		PlayerTwo.isTurn.classList.add('active');
+	} else {
+		PlayerTwo.isTurn.classList.remove('active');
+		PlayerOne.isTurn.classList.add('active');
+	}
+}
+
+let boxes = document.querySelectorAll('.box');
+
+for (let i = 0; i < boxes.length; i ++) {
+	boxes[i].addEventListener('mouseenter', function(e) {
+		boxes[i].style.backgroundImage = "url('img/o.svg')";
+	});
+	boxes[i].addEventListener('mouseleave', function(e) {
+		boxes[i].style.backgroundImage = "none";
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 goesFirst();
