c79d6bbd325dc9104f8a450324fa8c2597caf91b
theDuke523 <j.donmoyer523@gmail.com>
2018-04-04 16:43:33 -0400

    added hover and click functionality

--- tic-tac-toe-v3/js/tictacapp.js
+++ tic-tac-toe-v3/js/tictacapp.js
@@ -3,27 +3,47 @@
 const gameScreen = document.querySelector('#board');
 const startScreen = document.querySelector('#start');
 const startButton = document.querySelector('.button');
+const boxes = document.querySelectorAll('.box');
+let turns = 0;
+const winningCombos = [
+				["1", "2", "3"],
+				["4", "5", "6"],
+				["7", "8", "9"],
+				["1", "4", "7"],
+				["2", "5", "8"],
+				["3", "6", "9"],
+				["1", "5", "9"],
+				["3", "5", "7"]
+				];
+
+
 //const playerOne = document.querySelector('#player1');
 //const playerTwo = document.querySelector('#player2');
 
 let PlayerOne = {
 	isTurn: document.querySelector('#player1'),
-	isActive: false
+	isActive: false,
+	checkedBoxes: []
 };
 
 let PlayerTwo = {
 	isTurn: document.querySelector('#player2'),
-	isActive: false
+	isActive: false,
+	checkedBoxes: []
 };
 
-// Hides the game board on start
-gameScreen.style.display = "none";
+function startGame () {
+	// Hides the game board on start
 
-// Hides the start screen and displays the game board
-startButton.addEventListener('click', function() {
-	startScreen.style.display = 'none';
-	gameScreen.style.display = '';
-});
+	gameScreen.style.display = "none";
+
+	// Hides the start screen and displays the game board
+	startButton.addEventListener('click', function() {
+		startScreen.style.display = 'none';
+		gameScreen.style.display = '';
+	});
+	goesFirst();
+}
 
 // Function to pick who goes first
 function goesFirst () {
@@ -37,27 +57,84 @@ function goesFirst () {
 	}
 }
 
+
+
 // Function to change turns
-function switchTurn () {
-	if (PlayerTwo.isActive) {
-		PlayerOne.isTurn.classList.remove('active');
-		PlayerTwo.isTurn.classList.add('active');
-	} else {
-		PlayerTwo.isTurn.classList.remove('active');
-		PlayerOne.isTurn.classList.add('active');
+function switchTurn (x, y) {
+		x.isTurn.classList.remove('active');
+		y.isTurn.classList.add('active');
+		x.isActive = false;
+		y.isActive = true;
+}
+
+function isTie () {
+	if (turns === 9) {
+		alert("It's a Tie");
+		startScreen.style.display = '';
+		// PlayerOne.isTurn.classList.remove('active');
+		// PlayerTwo.isTurn.classList.remove('active');
+		// for (let i = 0; i <= turns; i++ ) {
+		// boxes[i].classList.remove('box-filled-1');
+		// boxes[i].classList.remove('box-filled-2');
+		// }
+		startGame();
 	}
 }
 
-let boxes = document.querySelectorAll('.box');
+// function isWinner (x) {
+// 	for (let i = 0; i < x.checkedBoxes; i++) {
+// 		if (x.checkedBoxes.sort()[0] === winningCombos[0][0] &&
+// 			x.checkedBoxes.sort()[1] === winningCombos[0][1] &&
+// 			x.checkedBoxes.sort()[2] === winningCombos[0][2]) {
+// 			console.log("Win");
+// 		} 
+// 	}
+// }
 
-for (let i = 0; i < boxes.length; i ++) {
-	boxes[i].addEventListener('mouseenter', function(e) {
-		boxes[i].style.backgroundImage = "url('img/o.svg')";
+// creates the basic functionality of the game
+
+for (let i = 0; i < boxes.length; i++) {
+
+	// event listener for mouseenter and mouseleave
+	boxes[i].addEventListener('mouseenter', function listen() {
+		if (!boxes[i].classList.contains('box-filled-1') && !boxes[i].classList.contains('box-filled-2')) {	
+		
+		if (PlayerOne.isActive) {
+			boxes[i].style.backgroundImage = "url('img/o.svg')";
+		} else if (PlayerTwo.isActive) {
+			boxes[i].style.backgroundImage = "url('img/x.svg')";
+		}
+	}
 	});
-	boxes[i].addEventListener('mouseleave', function(e) {
-		boxes[i].style.backgroundImage = "none";
-	});
-}
+	boxes[i].addEventListener('mouseleave', function() {
+			boxes[i].style.backgroundImage = "";
+			});
+
+	
+	// event listener for selecting boxes
+	boxes[i].addEventListener('click', function click() {
+		if (PlayerOne.isActive) {
+			boxes[i].classList.add('box-filled-1');
+			PlayerOne.checkedBoxes.push(boxes[i].id);
+			turns++
+			//isWinner(PlayerOne);
+			switchTurn(PlayerOne, PlayerTwo);
+			
+		} else if (PlayerTwo.isActive) {
+			boxes[i].classList.add('box-filled-2');
+			PlayerTwo.checkedBoxes.push(boxes[i].id);
+			turns++
+			//isWinner(PlayerTwo);
+			switchTurn(PlayerTwo, PlayerOne);	
+			
+		}
+			boxes[i].removeEventListener('click', click);
+			isTie();
+			});
+		};
+	
+
+startGame();
 
 
 
@@ -75,4 +152,4 @@ for (let i = 0; i < boxes.length; i ++) {
 
 
 
-goesFirst();
+
